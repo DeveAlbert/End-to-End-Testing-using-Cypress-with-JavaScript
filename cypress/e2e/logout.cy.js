acf8cfb455e87726cf877c7542e54a2d43aa2d94
DeveAlbert <albertemmanuel29@gmail.com>
2023-11-16 16:58:10 +0100

    Update: Added test for logout

--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -2,23 +2,13 @@ describe('logout of application under test', () => {
 
     it('should logout a standard user', () => {
         cy.fixture('elements').then((locator) => {
-            cy.Login(locator.standard)
             cy.Logout(locator.standard)
-        })
-    })
-
-
-    it('should logout a locked out user', () => {
-        cy.fixture('elements').then((locator) => {
-            cy.Login(locator.locked)
-            cy.Logout(locator.locked)
         })
     })
 
 
     it('should logout a problem user', () => {
         cy.fixture('elements').then((locator) => {
-            cy.Login(locator.problem)
             cy.Logout(locator.problem)
         })
     })
@@ -26,7 +16,6 @@ describe('logout of application under test', () => {
 
     it('should logout a performance glitch user', () => {
         cy.fixture('elements').then((locator) => {
-            cy.Login(locator.glitch)
             cy.Logout(locator.glitch)
         })
     })
@@ -34,7 +23,6 @@ describe('logout of application under test', () => {
 
     it('should logout an error user', () => {
         cy.fixture('elements').then((locator) => {
-            cy.Login(locator.error)
             cy.Logout(locator.error)
         })
     })
@@ -42,7 +30,6 @@ describe('logout of application under test', () => {
 
     it('should logout a visual user', () => {
         cy.fixture('elements').then((locator) => {
-            cy.Login(locator.visual)
             cy.Logout(locator.visual)
         })
     })
--- cypress/support/logout.js
+++ cypress/support/logout.js
@@ -1,6 +1,11 @@
-Cypress.Commands.add('Logout', () => {
+Cypress.Commands.add('Logout', (username) => {
     cy.fixture('elements').then((locator) => {
+        cy.get(locator.usernameField).should('be.visible').fill(username)
+        cy.get(locator.passwordField).should('be.visible').fill(locator.validPassword)
+        cy.get(locator.loginButton).should('be.visible').click()
+        cy.url().should('include', "/inventory.html")
         cy.get(locator.menuButton).should('be.visible').click()
         cy.get(locator.logoutButton).should('be.visible').click()
+        cy.url().should('eq', 'https://www.saucedemo.com/')
     })
 })
