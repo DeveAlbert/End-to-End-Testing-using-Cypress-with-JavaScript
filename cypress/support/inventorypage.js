0635f6cfd1ba17420d48a816c71c4c72f018bc44
DeveAlbert <albertemmanuel29@gmail.com>
2023-11-16 17:03:45 +0100

    Update: Test for Inventory Page

--- cypress/support/inventorypage.js
+++ cypress/support/inventorypage.js
@@ -1,13 +1,12 @@
-Cypress.Commands.add('AddSauceLabsBikeLightToCart', () => {
-    cy.fixture('elements').then((locator) => {
-        cy.get(locator.addToCartSauceLabsBikeLight).should('be.visible').click();
-    })
+Cypress.Commands.add('AddProductToCart', (product) => {
+    let productName = product.toLowerCase().split(" ").join("-")
+    cy.get(`[data-test=add-to-cart-${productName}]`).click()
 })
 
-Cypress.Commands.add('AddSauceLabsBackpackToCart', () => {
-    cy.fixture('elements').then((locator) => {
-        cy.get(locator.addSauceLabsBackpackToCart).should('be.visible').click();
-    })
+
+Cypress.Commands.add('RemoveProductFromCart', (product) => {
+    let productName = product.toLowerCase().split(" ").join("-")
+    cy.get(`[data-test=remove-${productName}]`).click()
 })
 
 
@@ -16,3 +15,4 @@ Cypress.Commands.add('NavigateToCart', () => {
         cy.get(locator.shoppingCartLink).should('be.visible').click()
     })
 })
+
